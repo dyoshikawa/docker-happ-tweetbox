3df2d20735ebf4840f1c77d5b8dbdb782cef68cd
dyoshikawa <yoshikawada@crunchtimer.jp>
2018-01-22 10:27:54 +0900

    fix

--- hyperapp/happ-entrypoint.sh
+++ hyperapp/happ-entrypoint.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/sh
-sudo mv /var/www/happ-pre/* /var/www/happ/
-sudo mv /var/www/happ-pre/.??* /var/www/happ/
-sudo rmdir /var/www/happ-pre
+if [ -f /var/www/happ/package.json ]; then
+    sudo rm -rf /var/www/happ-pre
+    sudo npm install
+    sudo npm run build
+else
+    sudo mv /var/www/happ-pre/* /var/www/happ/
+    sudo mv /var/www/happ-pre/.??* /var/www/happ/
+    sudo rmdir /var/www/happ-pre
+fi
 sudo chown -R 1000:1000 /var/www/happ
 sudo nginx -g 'daemon off;'
