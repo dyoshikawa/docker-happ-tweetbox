e9e75a63e2966a5c9ebe0ca61256cd6181e5142b
dyoshikawa <yoshikawada@crunchtimer.jp>
2018-01-22 11:43:42 +0900

    add fix

--- volumes/hyperapp.var-www-happ/src/js/main.js
+++ volumes/hyperapp.var-www-happ/src/js/main.js
@@ -1,24 +1,70 @@
 import "../../node_modules/jquery/dist/jquery.js"
 import "../../node_modules/popper.js/dist/umd/popper.js"
 import "../../node_modules/bootstrap/dist/js/bootstrap.js"
-
 import { h, app } from "hyperapp"
 
+const MAX_LENGTH = 120;
+const OFFSET = 10;
+
+const OverflowWidget = ({ text, offset, count }) => (
+  <div class="overflow">
+    <h1>Whoops! Too long.</h1>
+    <p>
+      ...{text.slice(0, offset)}
+      <span class="overflow-text">{text.slice(count)}</span>
+    </p>
+  </div>
+);
+
+const Tweetbox = ({ count, text, update }) => (
+  <main>
+    <div class="container">
+      <ul class="flex-outer">
+        <li>
+          <textarea placeholder="What's up?" value={text} oninput={update} />
+        </li>
+
+        <li class="flex-inner">
+          <span class={count > OFFSET ? "" : "overflow-count"}>{count}</span>
+
+          <button
+            onclick={() => alert(text)}
+            disabled={count >= MAX_LENGTH || count < 0}
+          >
+            Tweet
+          </button>
+        </li>
+      </ul>
+
+      {count < 0 && (
+        <OverflowWidget
+          text={text.slice(count - OFFSET)}
+          offset={OFFSET}
+          count={count}
+        />
+      )}
+    </div>
+  </main>
+);
+
 const state = {
-  count: 0
+  text: "",
+  count: MAX_LENGTH
 };
 
 const actions = {
-  down: () => state => ({ count: state.count - 1 }),
-  up: () => state => ({ count: state.count + 1 })
+  update: text => state => ({
+    text,
+    count: state.count + state.text.length - text.length
+  })
 };
 
 const view = (state, actions) => (
-<main>
-<h1>{state.count}</h1>
-<button onclick={actions.down}>-</button>
-<button onclick={actions.up}>+</button>
-</main>
+  <Tweetbox
+    text={state.text}
+    count={state.count}
+    update={e => actions.update(e.target.value)}
+  />
 );
 
 const main = app(state, actions, view, document.body);
